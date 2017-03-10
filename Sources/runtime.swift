5c40d68df3330b9885526b065fe057899d5149d4
Marcio Klepacz <marcioklepacz@gmail.com>
2017-03-10 09:37:47 -0300

    Remove all runtime JS Core things

--- Sources/runtime.swift
+++ Sources/runtime.swift
@@ -7,7 +7,7 @@
 //
 
 import Foundation
-import JavaScriptCore
+// import JavaScriptCore
 
 enum JSRuntimeError: Error {
     case objectsDontExist(String)
@@ -15,7 +15,7 @@ enum JSRuntimeError: Error {
 }
 
 struct JSRuntime {
-    private let context = JSContext()!
+    // private let context = JSContext()!
     
     init() {
         self.registerHelpers()
@@ -25,46 +25,47 @@ struct JSRuntime {
         // self["bubulu"] = Bubulu.self
     }
     
-    public subscript(key: String) -> Any? {
-        set {
-            self.context.setObject(newValue, forKeyedSubscript: key as (NSCopying & NSObjectProtocol)!)
-        }
-        get {
-            return self.context.objectForKeyedSubscript(key)
-        }
-    }
+    // public subscript(key: String) -> Any? {
+    //     set {
+    //         // self.context.setObject(newValue, forKeyedSubscript: key as (NSCopying & NSObjectProtocol)!)
+    //     }
+    //     get {
+    //         // return self.context.objectForKeyedSubscript(key)
+    //     }
+    // }
     
     func evaluate(_ body: String, params: [String: String]) throws -> Content {
         let paramsDeclaration = params.keys.map{ String($0.characters.dropFirst()) }.joined(separator: ",")
         
         let jsSource = "var mainFunc = function(\(paramsDeclaration)) { \(body) }"
         
-        _ = context.evaluateScript(jsSource)
+        // _ = context.evaluateScript(jsSource)
         
-        guard let mainFunc = self["mainFunc"] as? JSValue,
-            let result = mainFunc.call(withArguments: Array(params.values)) else  {
-                throw JSRuntimeError.objectsDontExist("mainFunc and result is nil")
-        }
-        
-        guard !result.isUndefined else {
-            throw JSRuntimeError.undefined
-        }
-        
-        return try JSRuntime.parse(result)
+        // guard let mainFunc = self["mainFunc"] as? JSValue,
+        //     let result = mainFunc.call(withArguments: Array(params.values)) else  {
+        //         throw JSRuntimeError.objectsDontExist("mainFunc and result is nil")
+        // }
+        //
+        // guard !result.isUndefined else {
+        //     throw JSRuntimeError.undefined
+        // }
+        //
+        // return try JSRuntime.parse(result)
+        return Content.html("Shalom")
     }
     
     
-    private static func parse(_ result: JSValue) throws -> Content {
-        
-        if result.isObject {
-            let json = result.toObject()! as! [String: Any]
-            let jsonData = try JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
-            
-            return Content.json(jsonData)
-            
-        }
-        
-        return Content.html(String(describing: result))
-    }
+    // private static func parse(_ result: JSValue) throws -> Content {
+    //     
+    //     if result.isObject {
+    //         let json = result.toObject()! as! [String: Any]
+    //         let jsonData = try JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
+    //         
+    //         return Content.json(jsonData)
+    //         
+    //     }
+    //     
+    //     return Content.html(String(describing: result))
+    // }
 }
 
